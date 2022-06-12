b0682d2662b3aca257cc6ea32b1d6c5989c8874f
Elias Wagner <elias-wagner.online@gmx.de>
2022-06-12 17:49:25 +0200

    added files

---
+++ Code/BSPTree_draft3/BSPTree_draft3.cpp
@@ -0,0 +1,39 @@
+// BSPTree_draft3.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
+//
+
+#include <iostream>
+#include "TreeNode.h"
+
+
+int main()
+{
+    dList<int> * l = new dList<int>(10);
+
+
+    for (int i = 0; i < 40; i++) {
+        int number = i;
+        l->addItem( number );
+    }
+    
+    for (int i = 0; i < l->getItemCount(); i++) {
+        int number = *l->getItem(i);
+        std::cout << i << ": " << number << std::endl;
+    }
+
+
+
+
+
+    std::cout << "Hello World!\n";
+}
+
+// Programm ausführen: STRG+F5 oder Menüeintrag "Debuggen" > "Starten ohne Debuggen starten"
+// Programm debuggen: F5 oder "Debuggen" > Menü "Debuggen starten"
+
+// Tipps für den Einstieg: 
+//   1. Verwenden Sie das Projektmappen-Explorer-Fenster zum Hinzufügen/Verwalten von Dateien.
+//   2. Verwenden Sie das Team Explorer-Fenster zum Herstellen einer Verbindung mit der Quellcodeverwaltung.
+//   3. Verwenden Sie das Ausgabefenster, um die Buildausgabe und andere Nachrichten anzuzeigen.
+//   4. Verwenden Sie das Fenster "Fehlerliste", um Fehler anzuzeigen.
+//   5. Wechseln Sie zu "Projekt" > "Neues Element hinzufügen", um neue Codedateien zu erstellen, bzw. zu "Projekt" > "Vorhandenes Element hinzufügen", um dem Projekt vorhandene Codedateien hinzuzufügen.
+//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "Datei" > "Öffnen" > "Projekt", und wählen Sie die SLN-Datei aus.
